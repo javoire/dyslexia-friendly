f880838d2f5c685cd75fa041e7ba193f24879eef
Jonatan Dahl <jo_da12@hotmail.com>
2013-11-23 19:06:33 +0100

    cleanup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,24 +6,5 @@ console.log('background.js loaded');
 // chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 chrome.extension.onMessage.addListener(function(message) {
     console.log('click event in popup received', message);
-
-    // switch(message.type) {
-    //     case 'font-family':
-    //         // console.log('changing font family to', message.value);
-    //         // var css = '* { font-family: "'+message.value+'" !important; }';
-    //         // console.log('injecting css', css);
-    //         // chrome.tabs.insertCSS("* { background-color: #000 !important }");
-    //         break;
-    // }
-
-    // chrome.tabs.getSelected(null, function(tab){
-    //     var msg = {
-    //         type: message.type,
-    //         value: message.value
-    //     };
-    //     console.log('background.js sending message to content script', msg);
-    //     chrome.tabs.sendMessage(tab.id, msg);
-    // });
-
     return true;
 });
