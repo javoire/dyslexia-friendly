8e67f26987da60f33c20db4a89c9018e19ecbbf2
Jonatan Dahl <jo_da12@hotmail.com>
2013-11-23 18:16:28 +0100

    added ruler that follows cursor vertically

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,6 +1,22 @@
 'use strict';
 
-console.log('content script loaded');
+console.log('[CONTENT SCRIPT] content script loaded');
+
+$(document).ready(function(){
+    console.log('[CONTENT SCRIPT] document loaded and jquery is with us!');
+    
+    //-------------------------------
+    // Ruler that follows mouse vertically
+    //-------------------------------
+    $.get(chrome.extension.getURL('/ruler.html'), function(data) {
+        console.log('ruler html', data);
+        $(data).appendTo('body');
+    });
+    $('body').mousemove(function(event) {
+        $("#ruler").css('top', event.pageY - 30);
+        $("#ruler").css('width', $(window).innerWidth());
+    });
+});
 
 // var all = document.getElementsByTagName('*');
 
