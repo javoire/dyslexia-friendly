6e3e508af45b14b68e6560e36a8411e8efc1a932
Jonatan Dahl <dahl.jonatan@gmail.com>
2024-07-01 18:52:47 -0400

    fonts urls are proprely writte for web and extension loading

--- src/website/js/index.js
+++ src/website/js/index.js
@@ -1,2 +1,8 @@
+import 'tw-elements';
+import 'jquery';
+
+import '../../shared/css/tailwind.css';
+import '../../shared/css/fonts.css';
+
 // eslint-disable-next-line no-console
 console.log('Hello website!');
