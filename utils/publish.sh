c52dc874940b98cd5a51909e5b0d06891d7bb499
Jonatan Dahl <dahl.jonatan@gmail.com>
2024-07-03 00:05:33 -0400

    Update publish.sh

--- utils/publish.sh
+++ utils/publish.sh
@@ -4,6 +4,7 @@ set -e
 
 # Upload and publish to Chrome Web Store
 
+echo "------- Init -------"
 ACCESS_TOKEN=$(curl --fail "https://accounts.google.com/o/oauth2/token" -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
 echo "------- Upload -------"
 curl --fail -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-goog-api-version: 2" -X PUT -T "${PACKAGE_NAME}" "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APP_ID}"
