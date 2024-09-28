7bda8d2a699a2761c8423aebb82dba74f88c131e
Jonatan Dahl <dahl.jonatan@gmail.com>
2024-09-28 18:58:26 +0200

    rm log

--- src/website/js/index.js
+++ src/website/js/index.js
@@ -4,6 +4,3 @@ import 'jquery';
 import '../../shared/css/tailwind.css';
 import '../../shared/css/fonts.css';
 import '../css/index.css';
-
-// eslint-disable-next-line no-console
-console.log('Hello website!');
