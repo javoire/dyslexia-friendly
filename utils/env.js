62a9b1b9f7b31ef1d918b7b69bb5eea0a9f58306
Jonatan Dahl <dahl.jonatan@gmail.com>
2025-01-26 17:48:21 +0800

    allow for interacting with popup in devserver

--- utils/env.js
+++ utils/env.js
@@ -1,6 +1,8 @@
+let nodeEnv = process.env.NODE_ENV || 'development';
 export default {
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: nodeEnv,
   IS_DEV: process.env.NODE_ENV !== 'production',
-  LOG_LEVEL: process.env.LOG_LEVEL || 'error',
-  PORT: process.env.PORT || 3000
+  LOG_LEVEL:
+    process.env.LOG_LEVEL || nodeEnv === 'development' ? 'debug' : 'error',
+  PORT: process.env.PORT || 3000,
 };
